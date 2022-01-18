580b70ffd8fe721f614fdc4f66fc59cdca6a63b2
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-18 15:52:27 -0600

    Wrote schema and model functions

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,24 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars');
+
 }
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
+  return db('fruits').where('id', id).first();
 }
 
-const create = () => {
+const create = (car) => {
   // DO YOUR MAGIC
+  const [id] = await db('car').insert(car);
+  return getById(id);
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
